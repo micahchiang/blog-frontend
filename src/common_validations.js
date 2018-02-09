4caf37e6d865ae8d02864a57c57debb74b8d0cc8
Micah Chiang <micahkchiang@gmail.com>
2018-02-09 11:20:40 -0800

    added common validation file...do not actually use these if you are looking at them.

---
+++ src/common_validations.js
@@ -0,0 +1,10 @@
+let patterns = {
+    'title': '^[A-Za-z0-9&!]+$',
+    'date': '^(0[1-9]|1[0-2]).(0[1-9]|1\\d|2\\d|3[01]).(20[\\d]{2})$'
+};
+
+export let validateAgainstPattern = function(pattern, val) {
+    let regex = new RegExp(patterns[pattern]);
+    return regex.test(val);
+};
+
