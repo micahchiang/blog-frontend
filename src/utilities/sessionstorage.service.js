932b6a852ae49c56932c79542de167d98735810d
Micah Chiang <micahkchiang@gmail.com>
2018-03-25 19:04:28 -0700

    remove unused file

--- src/utilities/sessionstorage.service.js
+++ src/utilities/sessionstorage.service.js
@@ -2,15 +2,6 @@ export default class SessionStorage {
 
     constructor() {
         this.sessionStorage = window.sessionStorage;
-        // let liveEntries;
-
-        // this.getSessionStorage = () => {
-        //     return liveEntries || JSON.parse(sessionStorage.getItem(name) || '[]');
-        // };
-        //
-        // this.setSessionStorage = (entries) => {
-        //     sessionStorage.setItem(name, JSON.stringify(liveEntries = entries));
-        // };
     }
 
     getSessionStorage(key) {
